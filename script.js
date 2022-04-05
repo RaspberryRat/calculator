634ee48ba3d8795c1ef92c80829884653060b913
Graham <git@grahamirvine.com>
2022-04-05 16:03:08 -0600

    Created operate function that can call all other math functions

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-//start iwht add subsctract mult divide functions
+//sCreate a new function operate that 
+//takes an operator and 2 numbers and then calls one of the above functions on the numbers.
 
 function addition(a, b) {
   return a + b;
@@ -14,4 +15,19 @@ function multiply(a, b) {
 
 function divide(a, b) {
   return a / b;
+}
+
+
+function operate(a, b, c) {
+  if (c === 'add') {
+    return addition(a, b);
+  } else if (c === 'sub') {
+    return substract(a, b);
+  } else if (c === 'mult') {
+    return multiply(a,b);
+  } else if (c === 'div') {
+    return divide(a,b);
+  } else {
+    return 'ERROR!'
+  }
 }
