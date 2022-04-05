ade413f41b0e0524873987dbae2c2893a426c70d
Graham <git@grahamirvine.com>
2022-04-05 15:43:00 -0600

    created basic html for page and basic add/multi/sub/div functions

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # calculator
+
+Goal is to create a working calculator on a webpage. Will use HTML and CSS for layout and design. Calculator functions
+will be programmed with javascript.
+
+Assignment for Odin project.
---
+++ script.js
@@ -0,0 +1,17 @@
+//start iwht add subsctract mult divide functions
+
+function addition(a, b) {
+  return a + b;
+}
+
+function substract (a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
