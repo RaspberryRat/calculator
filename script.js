00638d483177a36fefc38e314c2d556de4cd632b
Graham <git@grahamirvine.com>
2022-04-05 17:09:26 -0600

    Created coding for buttons to call function. Function it is calling isnt working

--- script.js
+++ script.js
@@ -23,11 +23,47 @@ function operate(a, b, c) {
     return addition(a, b);
   } else if (c === 'sub') {
     return substract(a, b);
-  } else if (c === 'mult') {
+  } else if (c === 'multiply') {
     return multiply(a,b);
-  } else if (c === 'div') {
+  } else if (c === 'divide') {
     return divide(a,b);
   } else {
     return 'ERROR!'
   }
 }
+
+const btns = document.querySelectorAll('button');
+
+
+btns.forEach(button =>
+  button.addEventListener('click', () => {
+    saveInput(button.id);
+  }));
+let arr = [];
+function saveInput(userInput) {
+    console.log(arr);
+    if (userInput === 'add' || userInput === 'sub' || userInput === 'multiply' || userInput === 'divide') {
+      arr.push(userInput);
+      console.log('array is ' + arr);
+    } else {
+      let num = parseInt(userInput);
+      arr.unshift(num);
+      console.log('ARRAY is ' + arr);
+      console.log('ARRAY length is ' + arr.length);
+    }
+    if (arr.length === 3) {
+      console.log(arr.length);
+      let a = parseInt(arr[0]);
+      let b = parseInt(arr[1]);
+      let c = arr[2];
+      console.log(typeof(c));
+      console.log('user input is ' + a, + b, + c);
+      console.log(typeof(a), typeof(b), typeof(c));
+      arr = [];
+      return operate(a, b, c);
+      
+    }
+  }
+
+
+
