aa79e6fdd45b5cc23535ef0dd9786be7c4fcd2b9
Graham <git@grahamirvine.com>
2022-04-06 08:28:08 -0600

    Fixed operate function. Created new function to display answer in box above calculator

--- script.js
+++ script.js
@@ -2,19 +2,23 @@
 //takes an operator and 2 numbers and then calls one of the above functions on the numbers.
 
 function addition(a, b) {
-  return a + b;
+  let ans = a + b;
+  displayAns(ans);
 }
 
 function substract (a, b) {
-  return a - b;
+  let ans = a - b;
+  displayAns(ans);
 }
 
 function multiply(a, b) {
-  return a * b;
+  let ans = a * b;
+  displayAns(ans);
 }
 
 function divide(a, b) {
-  return a / b;
+  let ans = a / b;
+  displayAns(ans);
 }
 
 
@@ -33,6 +37,7 @@ function operate(a, b, c) {
 }
 
 const btns = document.querySelectorAll('button');
+const display = document.querySelector('.display');
 
 
 btns.forEach(button =>
@@ -41,29 +46,34 @@ btns.forEach(button =>
   }));
 let arr = [];
 function saveInput(userInput) {
-    console.log(arr);
+    //console.log(arr);
     if (userInput === 'add' || userInput === 'sub' || userInput === 'multiply' || userInput === 'divide') {
       arr.push(userInput);
-      console.log('array is ' + arr);
+      //console.log('array is ' + arr);
     } else {
       let num = parseInt(userInput);
-      arr.unshift(num);
-      console.log('ARRAY is ' + arr);
-      console.log('ARRAY length is ' + arr.length);
-    }
+      arr.unshift(num); // need to figure out how to add second number to second spot in array
+      //console.log('ARRAY is ' + arr);
+      //console.log('ARRAY length is ' + arr.length);
+    }//
     if (arr.length === 3) {
-      console.log(arr.length);
+      //console.log(arr.length);
       let a = parseInt(arr[0]);
       let b = parseInt(arr[1]);
       let c = arr[2];
-      console.log(typeof(c));
-      console.log('user input is ' + a, + b, + c);
-      console.log(typeof(a), typeof(b), typeof(c));
+      //console.log(typeof(c));
+      //console.log('user input is ' + a, + b, + c);
+      //console.log(typeof(a), typeof(b), typeof(c));
       arr = [];
-      return operate(a, b, c);
+      operate(a, b, c);
       
     }
   }
+
+  function displayAns(ans) {
+    display.textContent = `${ans}`;
+  }
+  
 
 
 
